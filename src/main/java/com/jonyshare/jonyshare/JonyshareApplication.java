19629ef10b81ebd369cadecd045295255eed9f47
DanielWang <danielwang99@foxmail.com>
2020-10-07 10:40:59 +0800

    spring 初始项目

---
+++ src/main/java/com/jonyshare/jonyshare/JonyshareApplication.java
@@ -0,0 +1,13 @@
+package com.jonyshare.jonyshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonyshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JonyshareApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonyshare/jonyshare/JonyshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonyshare.jonyshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonyshareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
