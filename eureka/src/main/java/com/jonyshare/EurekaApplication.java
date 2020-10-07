cb8c9cad1d409322b2924cd654c63294194f9433
DanielWang <danielwang99@foxmail.com>
2020-10-07 11:35:05 +0800

    1. 解决了因为配置问题，eureka启动失败的问题,修复启动后访问404问题（没加@EnableEurekaServer注解） 2. 加入logback日志系统

--- eureka/src/main/java/com/jonyshare/EurekaApplication.java
+++ eureka/src/main/java/com/jonyshare/EurekaApplication.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.core.env.Environment;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
     private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
