2824f695893b6478f48672fe35810b9b82ec3c67
DanielWang <danielwang99@foxmail.com>
2020-10-07 11:29:11 +0800

    1. 解决了因为配置问题，eureka启动失败的问题 2. 加入logback日志系统

--- eureka/src/main/java/com/jonyshare/EurekaApplication.java
+++ eureka/src/main/java/com/jonyshare/EurekaApplication.java
@@ -1,13 +1,25 @@
 package com.jonyshare;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EurekaApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Eureka地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
 
 }
