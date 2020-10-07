97716fed8386e2aae3d19a046bad20d8d6e5f2d8
DanielWang <danielwang99@foxmail.com>
2020-10-07 20:46:29 +0800

    1. 创建gateway模块并且将gateway模块注册到eureka

---
+++ gateway/src/main/java/com/jonyshare/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.jonyshare.gateway;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
