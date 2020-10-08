9f32c1da46c8f4fce5724a596dcb3240237af3de
DanielWang <danielwang99@foxmail.com>
2020-10-08 12:11:06 +0800

    1.新增server工共模块

---
+++ server/src/main/java/com/jonyshare/server/mapper/TestMapper.java
@@ -0,0 +1,15 @@
+package com.jonyshare.server.mapper;
+
+
+
+import com.jonyshare.server.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author WangQiang
+ * @date 2020/10/8-9:02
+ */
+public interface TestMapper {
+    public List<Test> list();
+}
---
+++ server/src/main/java/com/jonyshare/server/service/TestService.java
@@ -0,0 +1,24 @@
+package com.jonyshare.server.service;
+
+
+import com.jonyshare.server.domain.Test;
+import com.jonyshare.server.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author WangQiang
+ * @date 2020/10/8-9:18
+ */
+@Service
+public class TestService {
+
+    @Resource
+    TestMapper testMapper;
+
+    public List<Test> list() {
+        return testMapper.list();
+    }
+}
