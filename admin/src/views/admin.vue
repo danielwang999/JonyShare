a7be19481523f2bcbe63160176bb477c4ab677bf
DanielWang <danielwang99@foxmail.com>
2020-10-15 15:20:26 +0800

    1.增加welcome子组件，增加/admin/welcome子路由 router-view和子路由配合使用 注意：子路由的配置不要'/'开头

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -933,7 +933,7 @@
             <div class="row">
               <div class="col-xs-12">
                 <!-- PAGE CONTENT BEGINS -->
-
+                  <router-view/>
                 <!-- PAGE CONTENT ENDS -->
               </div><!-- /.col -->
             </div><!-- /.row -->
---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>您好，欢迎进入控台！</h1>
+  </div>
+</template>
