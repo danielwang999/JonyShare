be10544e6e30e3691f1f37e11ebcefc0eca2e7fa
DanielWang <danielwang99@foxmail.com>
2020-10-15 13:28:07 +0800

    1.安装vue-router 2.增加login页面(login.vue) 3.增加路由配置(router.js)访问/*,/,/login时，加载login组件

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import app from './app.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(app),
 }).$mount('#app')
