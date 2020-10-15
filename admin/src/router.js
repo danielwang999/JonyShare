92741513b9b5a714e57385571a158eed619217f2
DanielWang <danielwang99@foxmail.com>
2020-10-15 14:07:27 +0800

    1.修改login页面，改成中文 2.加入登录跳转到admin页面功能

--- admin/src/router.js
+++ admin/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import Router from "vue-router"
 import Login from "./views/login.vue"
+import Admin from "./views/admin.vue"
 
 Vue.use(Router);
 
@@ -16,5 +17,8 @@ export default new Router({
   }, {
     path: "/login",
     component: Login
+  }, {
+    path: "/admin",
+    component: Admin
   }]
 })
