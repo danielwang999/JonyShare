aee56fcfe1ade9c36f2b350bc765dd9eecc22022
DanielWang <danielwang99@foxmail.com>
2020-10-16 20:58:47 +0800

    1.使用gateway路由转发，vue页面只访问gateway的端口 2.把跨域配置放到gateway启动类下面

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -97,7 +97,7 @@
        */
       list(page) {
         let _this = this;
-        _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+        _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/list').then((response)=>{
           console.log("查询大章列表结果：", response);
           _this.chapters = response.data;
         })
--- gateway/src/main/java/com/jonyshare/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/jonyshare/gateway/GatewayApplication.java
@@ -6,7 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -19,6 +24,26 @@ public class GatewayApplication {
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！！");
         LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+    /**
+     * 配置跨域
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 
 }
