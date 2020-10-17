a3c19619c90fbbe762de68267f95772f2d5aa1de
DanielWang <danielwang99@foxmail.com>
2020-10-17 17:41:22 +0800

    1.实现刷新chapter按钮功能 2.增加了分页组件pagination.vue 3.大章管理页面使用分页组件，可自定义初始每页5条，最多显示8个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,85 +1,97 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-    <thead>
-    <tr>
+  <div>
+    <p>
+      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
+    </p>
+    <pagination ref="pagination" v-bind:list="list"></pagination>
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
 
-      <th>ID</th>
-      <th>名称</th>
-      <th>课程ID</th>
-      <th>操作</th>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
 
-      <th></th>
-    </tr>
-    </thead>
+        <th></th>
+      </tr>
+      </thead>
 
-    <tbody>
+      <tbody>
 
-    <tr v-for="chapter in chapters">
-      <td>{{chapter.id}}</td>
-      <td>{{chapter.name}}</td>
-      <td>{{chapter.courseId}}</td>
-      <td>
-        <div class="hidden-sm hidden-xs btn-group">
-          <button class="btn btn-xs btn-success">
-            <i class="ace-icon fa fa-check bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-info">
-            <i class="ace-icon fa fa-pencil bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-danger">
-            <i class="ace-icon fa fa-trash-o bigger-120"></i>
-          </button>
-
-          <button class="btn btn-xs btn-warning">
-            <i class="ace-icon fa fa-flag bigger-120"></i>
-          </button>
-        </div>
-
-        <div class="hidden-md hidden-lg">
-          <div class="inline pos-rel">
-            <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-              <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+      <tr v-for="chapter in chapters">
+        <td>{{chapter.id}}</td>
+        <td>{{chapter.name}}</td>
+        <td>{{chapter.courseId}}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
             </button>
 
-            <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-              <li>
-                <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
+              </button>
+
+              <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
+                  </a>
+                </li>
 
-              <li>
-                <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                </a>
-              </li>
-            </ul>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </td>
-    </tr>
+        </td>
+      </tr>
 
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
+  import Pagination from "../../components/pagination";
   export default {
     name: 'chapter',
+    components: {Pagination},
     data: function() {
       return {
         chapters: []
@@ -89,7 +101,7 @@
       // sidebar激活样式方法一
       // this.$parent.activeSidebar("business-chapter-sidebar");
       let _this = this;
-      _this.list();
+      _this.list(1);
     },
     methods: {
       /**
@@ -98,11 +110,12 @@
       list(page) {
         let _this = this;
         _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-          page: 1,
-          size: 5
+          page: page,
+          size: _this.$refs.pagination.size,
         }).then((response)=>{
           console.log("查询大章列表结果：", response);
           _this.chapters = response.data.list;
+          _this.$refs.pagination.render(page, response.data.total);
         })
         // Loading.show();
         // _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/chapter/list', {
