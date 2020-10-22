926d1a8c9922948e868c381bc7a119b7f43d4f39
DanielWang <danielwang99@foxmail.com>
2020-10-22 16:13:41 +0800

    1.小节表section的结构设计和sql执行 2.生成section表的持久层代码

--- doc/sql/all.sql
+++ doc/sql/all.sql
@@ -40,6 +40,28 @@ values ('00000013', '00000000', '测试大章13');
 insert into `chapter` (id, course_id, name)
 values ('00000014', '00000000', '测试大章14');
 
+
+-- 小节
+drop table if exists `section`;
+create table `section` (
+    `id` char(8) not null default '' comment 'id',
+    `title` varchar(50) not null comment '标题',
+    `course_id` char(8) comment '课程|course.id',
+    `chapter_id` char(8) comment '大章|chapter.id',
+    `video` varchar(200) comment '视频',
+    `time` int comment '时长|单位秒',
+    `charge` char(1) comment '收费|C 收费；F 免费',
+    `sort` int comment '顺序',
+    `created_at` datetime(3) comment '创建时间',
+    `updated_at` datetime(3) comment '修改时间',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='小节';
+
+insert into `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+values ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'f', 1, now(), now());
+
+
+
 -- 对test表测试的脚本
 drop table if exists 'test';
 create table 'test'
