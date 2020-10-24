d9f35464bbd61ed16feb43605f23eda4eae5d7c8
DanielWang <danielwang99@foxmail.com>
2020-10-24 15:53:35 +0800

    1.生成器开发：增加生成dto 2.代码生成器和mybatis-generator整合：利用读取generatorConfig.xml里面的table属性获得表名和字段名

--- generator/src/main/java/com/jonyshare/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jonyshare/generator/server/ServerGenerator.java
@@ -1,12 +1,15 @@
 package com.jonyshare.generator.server;
 
-import com.jonyshare.generator.test.Test;
-import com.jonyshare.generator.util.FreemarkerUtil;
-import freemarker.template.TemplateException;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import com.jonyshare.generator.util.DbUtil;
+import com.jonyshare.generator.util.Field;
+import com.jonyshare.generator.util.FreemarkerUtil;
+
+import java.io.File;
+import java.util.*;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 /**
  * @author WangQiang
@@ -15,18 +18,44 @@ import java.util.Map;
 public class ServerGenerator {
     static String MODULE = "business";
     static String toServicePath = "server\\src\\main\\java\\com\\jonyshare\\server\\service\\";
+    static String toDtoPath = "server\\src\\main\\java\\com\\jonyshare\\server\\dto\\";
     static String toControllerPath = MODULE + "\\src\\main\\java\\com\\jonyshare\\" + MODULE + "\\controller\\admin\\";
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
+    public static void main(String[] args) throws Exception {
         String module = MODULE;
+
+        // 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement= (Element) contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
+        Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
         map.put("domain", domain);
         map.put("tableNameCn", tableNameCn);
         map.put("module", module);
+        map.put("fieldList", fieldList);
+        map.put("typeSet", typeSet);
+
+        // 生成dto
+        FreemarkerUtil.initConfig("dto.ftl");
+        FreemarkerUtil.generator(toDtoPath + Domain + "Dto.java", map);
 
         // 生成service
         FreemarkerUtil.initConfig("service.ftl");
@@ -35,5 +64,17 @@ public class ServerGenerator {
         // 生成controller
         FreemarkerUtil.initConfig("controller.ftl");
         FreemarkerUtil.generator(toControllerPath + Domain + "Controller.java", map);
+    }
+
+    /**
+     * 获取所有的Java类型，使用Set去重
+     */
+    private static Set<String> getJavaTypes(List<Field> fieldList) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
+            set.add(field.getJavaType());
+        }
+        return set;
     }
 }
