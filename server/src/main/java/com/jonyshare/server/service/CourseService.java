511f13fad7316f953deb7be9d58474c750c0ea35
DanielWang <danielwang99@foxmail.com>
2020-11-05 17:30:04 +0800

    1.修复Bug：修复CourseService里面save()保存课程方法的bug，当新增课程时，courseDto的id是空的，应该使用course的id，在insert()方法里面给course的id赋值了

--- server/src/main/java/com/jonyshare/server/service/CourseService.java
+++ server/src/main/java/com/jonyshare/server/service/CourseService.java
@@ -64,7 +64,7 @@ public class CourseService {
         }
 
         // 批量保存分类
-        courseCategoryService.saveBatch(courseDto.getId(), courseDto.getCategorys());
+        courseCategoryService.saveBatch(course.getId(), courseDto.getCategorys());
     }
 
     /**
