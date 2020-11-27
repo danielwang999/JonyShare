387f7d5d4cffdbfc42ee0bf4b734e42800e162d7
DanielWang <danielwang99@foxmail.com>
2020-11-27 17:07:26 +0800

    文件上传功能开发： 11.上传文件(头像)时，保存文件记录到数据库

--- file/src/main/java/com/jonyshare/file/controller/admin/UploadController.java
+++ file/src/main/java/com/jonyshare/file/controller/admin/UploadController.java
@@ -1,7 +1,9 @@
 package com.jonyshare.file.controller.admin;
 
 
+import com.jonyshare.server.dto.FileDto;
 import com.jonyshare.server.dto.ResponseDto;
+import com.jonyshare.server.service.FileService;
 import com.jonyshare.server.util.UuidUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 
@@ -30,6 +33,9 @@ public class UploadController {
     @Value("${file.path}")
     private String FILE_PATH;
 
+    @Resource
+    private FileService fileService;
+
     @RequestMapping("/upload")
     public ResponseDto upload(@RequestParam MultipartFile file) throws IOException {
         LOG.info("上传文件开始：{}",file);
@@ -38,13 +44,24 @@ public class UploadController {
 
         String fileName = file.getOriginalFilename();
         String key = UuidUtil.getShortUuid();
-        String fullPath = FILE_PATH + "teacher/" + key + "-" + fileName;
+        String suffix = fileName.substring(fileName.indexOf(".") + 1).toLowerCase();
+        String path = "teacher/" + key + "." + suffix;
+        String fullPath = FILE_PATH + path;
         File dest = new File(fullPath);
         file.transferTo(dest);
         LOG.info(dest.getAbsolutePath());
 
+        LOG.info("保存文件记录开始");
+        FileDto fileDto = new FileDto();
+        fileDto.setPath(path);
+        fileDto.setName(fileName);
+        fileDto.setSize(Math.toIntExact(file.getSize()));
+        fileDto.setSuffix(suffix);
+        fileDto.setUse("");
+        fileService.save(fileDto);
+
         ResponseDto responseDto = new ResponseDto();
-        responseDto.setContent(FILE_DOMAIN + "f/teacher/" + key + "-" + fileName);
+        responseDto.setContent(FILE_DOMAIN + path);
         return responseDto;
     }
 }
