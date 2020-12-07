5c5bc8bb748f421c2a3ca508973bea5c152a7482
DanielWang <danielwang99@foxmail.com>
2020-12-07 23:15:16 +0800

    大文件断点续传功能开发： 8.在最后一个分片上传完后，调用merge()方法，把分片合成一个文件，切将分片删除，需要注意的时，删除前需要调用GC，不然可能删除失败。

--- file/src/main/java/com/jonyshare/file/controller/admin/UploadController.java
+++ file/src/main/java/com/jonyshare/file/controller/admin/UploadController.java
@@ -15,8 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * @author WangQiang
@@ -38,7 +37,7 @@ public class UploadController {
     private FileService fileService;
 
     @RequestMapping("/upload")
-    public ResponseDto upload(@RequestBody FileDto fileDto) throws IOException {
+    public ResponseDto upload(@RequestBody FileDto fileDto) throws IOException, InterruptedException {
         String use = fileDto.getUse();
         String key = fileDto.getKey();
         String suffix = fileDto.getSuffix();
@@ -53,28 +52,82 @@ public class UploadController {
         if (!fullDir.exists()) {
             fullDir.mkdir();
         }
-
         String path = new StringBuffer(dir)
                 .append(File.separator)
                 .append(key)
                 .append(".")
                 .append(suffix)
-                .append("-")
-                .append(fileDto.getShardIndex()).toString();
-
-        String fullPath = FILE_PATH + path;
+                .toString(); // course\6sfSqfOwzmik4A4icMYuUe.mp4
+        String localPath = new StringBuffer(path)
+                .append(".")
+                .append(fileDto.getShardIndex())
+                .toString(); // course\6sfSqfOwzmik4A4icMYuUe.mp4.1
+        String fullPath = FILE_PATH + localPath;
         File dest = new File(fullPath);
         shard.transferTo(dest);
+        
         LOG.info(dest.getAbsolutePath());
-
         LOG.info("保存文件记录开始");
 
         fileDto.setPath(path);
         fileService.save(fileDto);
 
+        // 如果该分片是最后一片，进行分片的合并
+        if (fileDto.getShardIndex().equals(fileDto.getShardTotal())) {
+            this.merge(fileDto);
+        }
+
         ResponseDto responseDto = new ResponseDto();
         fileDto.setPath(FILE_DOMAIN + path);
         responseDto.setContent(fileDto);
         return responseDto;
+    }
+
+    private void merge(FileDto fileDto) throws InterruptedException, FileNotFoundException {
+        LOG.info("合并分片开始");
+        String path = fileDto.getPath(); //http://127.0.0.1:9000/file/f/course\6sfSqfOwzmik4A4icMYuUe.mp4
+        path = path.replace(FILE_DOMAIN, ""); //course\6sfSqfOwzmik4A4icMYuUe.mp4
+        Integer shardTotal = fileDto.getShardTotal();
+        File newFile = new File(FILE_PATH + path);
+        FileOutputStream outputStream = new FileOutputStream(newFile, true);//文件追加写入
+        FileInputStream fileInputStream = null;//分片文件
+        byte[] byt = new byte[10 * 1024 * 1024];
+        int len;
+
+        try {
+            for (int i = 0; i < shardTotal; i++) {
+                // 读取第i个分片
+                fileInputStream = new FileInputStream(new File(FILE_PATH + path + "." + (i + 1))); //  course\6sfSqfOwzmik4A4icMYuUe.mp4.1
+                while ((len = fileInputStream.read(byt)) != -1) {
+                    outputStream.write(byt, 0, len);
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("分片合并异常", e);
+        } finally {
+            try {
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+                outputStream.close();
+                LOG.info("IO流关闭");
+            } catch (Exception e) {
+                LOG.error("IO流关闭", e);
+            }
+        }
+        LOG.info("合并分片结束");
+
+        System.gc();
+        Thread.sleep(100);
+
+        // 删除分片
+        LOG.info("删除分片开始");
+        for (int i = 0; i < shardTotal; i++) {
+            String filePath = FILE_PATH + path + "." + (i + 1);
+            File file = new File(filePath);
+            boolean result = file.delete();
+            LOG.info("删除{}，{}", filePath, result ? "成功" : "失败");
+        }
+        LOG.info("删除分片结束");
     }
 }
