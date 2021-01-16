090c7bdebe58ebe1e1cf40cec9ee70fd540131ff
DanielWang <danielwang99@foxmail.com>
2021-01-16 18:03:28 +0800

    完成前台网站编写(仅前端)

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">回到顶部</a>
+      </p>
+      <p>欢迎使用享教你在线视频课程系统</p>
+      <p>2099&nbsp;jonyshare.com&nbsp;&nbsp;京ICP备 857857857号</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'theFooter',
+  }
+</script>
